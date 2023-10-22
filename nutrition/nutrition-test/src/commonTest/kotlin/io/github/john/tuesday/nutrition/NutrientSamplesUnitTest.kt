ce3b79554ae568c81de6554949a834444a422464
John-Tuesday <10697715+John-Tuesday@users.noreply.github.com>
2023-10-22 13:26:58 -0500

    Add unit tests for nutrition-test

---
+++ nutrition/nutrition-test/src/commonTest/kotlin/io/github/john/tuesday/nutrition/NutrientSamplesUnitTest.kt
@@ -0,0 +1,28 @@
+package io.github.john.tuesday.nutrition
+
+import org.calamarfederal.physical.measurement.*
+import kotlin.test.Test
+import kotlin.test.assertNotEquals
+import kotlin.test.assertNotNull
+
+
+private fun assertAllFilled(foodNutrition: FoodNutrition, assertNonZero: Boolean = false) {
+    assertNotEquals(0.00, foodNutrition.foodEnergy.inKilocalories())
+    assertNotEquals(0.00, foodNutrition.portion.fold({ it.inGrams() }, { it.inMilliliters() }))
+    NutrientType.entries.onEach {
+        val actual = foodNutrition[it]
+        assertNotNull(actual, message = "Nutrient $it is null")
+        if (assertNonZero)
+            assertNotEquals(0.00, actual.inGrams(), message = "Expected nutrient $it not equal to 0.00, but found $actual")
+    }
+}
+
+class NutrientSamplesUnitTest {
+
+    @Test
+    fun `fill test`() {
+        assertAllFilled(NutritionSamples.filledNutritionA, assertNonZero = false)
+        assertAllFilled(NutritionSamples.filledNonZeroNutritionA, assertNonZero = true)
+        assertAllFilled(NutritionSamples.filledNutritionB, assertNonZero = false)
+    }
+}
