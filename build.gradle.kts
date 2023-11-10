a1874f7f007e48fa49879b7e4362055a68f3854b
John-Tuesday <10697715+John-Tuesday@users.noreply.github.com>
2023-11-10 17:27:57 -0600

    Configure dokkaHtmlMultiModule for the root project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,23 @@
+import io.github.john.tuesday.nutrition.MavenGroupId
+import io.github.john.tuesday.nutrition.MavenVersion
+
 plugins {
     alias(libs.plugins.dokka) apply true
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.maven.publish.assist) apply false
+    id("nutrition.kotlin.library.multiplatform") apply false
+    id("nutrition.maven.publication") apply false
+}
+
+group = MavenGroupId
+version = MavenVersion
+
+val dokkaHtmlMultiModule = tasks.dokkaHtmlMultiModule
+
+dokkaHtmlMultiModule {
+    outputDirectory = rootProject.layout.projectDirectory.dir("docs/documentation")
+}
+
+tasks.withType<PublishToMavenRepository>().configureEach {
+    dependsOn(dokkaHtmlMultiModule)
 }
