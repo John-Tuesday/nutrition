7046a0bfba784240eb924e4edfc67141e6c1064b
John-Tuesday <10697715+John-Tuesday@users.noreply.github.com>
2023-11-10 21:34:24 -0600

    Add documentation base url to RepositoryInfo (which should point to github pages documentation)

--- build-logic/convention/src/main/kotlin/io/github/john/tuesday/nutrition/ConfigurePublishing.kt
+++ build-logic/convention/src/main/kotlin/io/github/john/tuesday/nutrition/ConfigurePublishing.kt
@@ -12,18 +12,21 @@ public data class RepositoryInfo(
     val description: String,
     val repoUrl: String,
     val homeUrl: String = repoUrl,
+    val documentationBaseUrl: String = "",
 )
 
 public val NutritionRepo: RepositoryInfo = RepositoryInfo(
     name = "Nutrition",
     description = "Kotlin library which models human nutritional data",
     repoUrl = "https://github.com/John-Tuesday/nutrition",
+    documentationBaseUrl = "https://john-tuesday.github.io/nutrition/documentation/nutrition/nutrition",
 )
 
 public val NutritionTestRepo: RepositoryInfo = RepositoryInfo(
     name = "Nutrition Test",
     description = "Kotlin test helper library for Nutrition library",
     repoUrl = "https://github.com/John-Tuesday/nutrition",
+    documentationBaseUrl = "https://john-tuesday.github.io/nutrition/documentation/nutrition/nutrition-test",
 )
 
 internal fun MavenPomDeveloperSpec.johnTuesday() {
