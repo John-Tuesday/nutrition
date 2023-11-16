188268c694d4920e742b71828bd29b76467ee6a3
John-Tuesday <10697715+John-Tuesday@users.noreply.github.com>
2023-11-16 15:29:54 -0600

    Use new api to configure Sonatype Staging and GitHub Packages maven repositories for publication

--- build-logic/convention/src/main/kotlin/MavenPublicationConvention.kt
+++ build-logic/convention/src/main/kotlin/MavenPublicationConvention.kt
@@ -4,12 +4,15 @@ import io.github.john.tuesday.plugins.MavenPublishAssistPlugin
 import io.github.john.tuesday.plugins.publishing.model.LicensePreset
 import io.github.john.tuesday.plugins.publishing.model.MavenRepository
 import io.github.john.tuesday.plugins.publishing.model.license
+import io.github.john.tuesday.plugins.publishing.model.maven
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.credentials.PasswordCredentials
 import org.gradle.api.publish.PublishingExtension
 import org.gradle.api.publish.maven.MavenPublication
-import org.gradle.kotlin.dsl.*
+import org.gradle.kotlin.dsl.apply
+import org.gradle.kotlin.dsl.assign
+import org.gradle.kotlin.dsl.getByType
+import org.gradle.kotlin.dsl.withType
 
 class MavenPublicationConvention : Plugin<Project> {
     override fun apply(target: Project) {
@@ -20,10 +23,14 @@ class MavenPublicationConvention : Plugin<Project> {
 
             val publishing = extensions.getByType<PublishingExtension>()
             publishing.repositories {
-                maven {
-                    setUrl(MavenRepository.SonatypeStaging.url)
-                    credentials(PasswordCredentials::class)
-                }
+                maven(
+                    repo = MavenRepository.SonatypeStaging,
+                    providers = providers,
+                )
+                maven(
+                    repo = MavenRepository.GitHubPackage(owner = "john-tuesday", repository = "nutrition"),
+                    providers = providers,
+                )
             }
             publishing.publications.withType<MavenPublication>().configureEach {
                 pom {
