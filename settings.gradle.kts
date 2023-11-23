d1b771ec253f1b0802828e71cf950465be9f5f09
John-Tuesday <10697715+John-Tuesday@users.noreply.github.com>
2023-11-23 13:36:24 -0600

    Clean up old comment

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,7 +5,7 @@ pluginManagement {
         gradlePluginPortal()
         maven {
             name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/john-tuesday/*")//gradle-convention-plugins
+            url = uri("https://maven.pkg.github.com/john-tuesday/*")
             credentials {
                 username = providers.gradleProperty("gpr.user").orElse(providers.environmentVariable("USERNAME")).get()
                 password = providers.gradleProperty("gpr.key").orElse(providers.environmentVariable("TOKEN")).get()
