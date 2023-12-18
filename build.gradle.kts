1ad050d868e6515f14db71b00678030367d7be69
John-Tuesday <10697715+John-Tuesday@users.noreply.github.com>
2023-12-17 23:31:17 -0600

    Add dokka versioning convention plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,12 @@ import io.github.john.tuesday.nutrition.MavenGroupId
 import io.github.john.tuesday.nutrition.MavenVersion
 
 plugins {
-    alias(libs.plugins.dokka.convention.html) apply true
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.maven.publish.assist) apply false
     id("nutrition.kotlin.library.multiplatform") apply false
     id("nutrition.maven.publication") apply false
+    alias(libs.plugins.dokka.convention.html)
+    alias(libs.plugins.dokka.convention.versioning)
 }
 
 group = MavenGroupId
