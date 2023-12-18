dfd709acc8c6370f55699b45618edc9254712952
John-Tuesday <10697715+John-Tuesday@users.noreply.github.com>
2023-12-17 22:59:59 -0600

    Change environment variable name
    Add more clear error. Also check system properties.

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -6,8 +6,16 @@ dependencyResolutionManagement {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/john-tuesday/*")
             credentials {
-                username = providers.gradleProperty("gpr.user").orElse(providers.environmentVariable("USERNAME")).get()
-                password = providers.gradleProperty("gpr.key").orElse(providers.environmentVariable("TOKEN")).get()
+                username = providers
+                    .gradleProperty("gpr.user")
+                    .orElse(providers.systemProperty("gpr.user"))
+                    .orElse(providers.environmentVariable("GPR_USERNAME"))
+                    .orNull ?: error("Expected property 'gpr.user' or environment variable 'GPR_USERNAME'")
+                password = providers
+                    .gradleProperty("gpr.key")
+                    .orElse(providers.systemProperty("gpr.key"))
+                    .orElse(providers.environmentVariable("GPR_TOKEN"))
+                    .orNull ?: error("Expected property 'gpr.key' or environment variable 'GPR_TOKEN'")
             }
         }
     }
